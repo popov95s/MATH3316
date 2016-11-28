a316f23a4e995028da5f9f9fc4b299123a6be189
popov95s <spopov@smu.edu>
2016-11-27 22:32:43 -0600

    part 1 written out

--- Project4/composite_int.cpp
+++ Project4/composite_int.cpp
@@ -11,34 +11,35 @@ using namespace std;
 
 
 
-double composite_int(Fcn & f, const double a,const double b,const int n){
-	//get midpoint
-	double h = (b-a)/n;
+	double composite_int(Fcn & f, const double a,const double b,const int n){
+		//get midpoint
+		double h = (b-a)/n;
 
-	//set weights
-	double x1 = -0.86113631159405;
-	double x2 = -0.33998104358486;
-	double x3 = 0.33998104358486;
-	double x4 = 0.86113631159405;
-	double w1 = 0.34785484513745;
-	double w2 = 0.65214515486254;
-	double w3 = 0.65214515486255;
-	double w4 = 0.34785484513745;
+		//set weights
+		double x1 = -0.86113631159405;
+		double x2 = -0.33998104358486;
+		double x3 = 0.33998104358486;
+		double x4 = 0.86113631159405;
+		double w1 = 0.34785484513745;
+		double w2 = 0.65214515486254;
+		double w3 = 0.65214515486255;
+		double w4 = 0.34785484513745;
 
-	double result=0.0;
+		double result=0.0;
 
-	double xmid,node1, node2,node3,node4;
+		double xmid,node1, node2,node3,node4;
 
-	for(int i=0 ;i<n;i++){
+		//iterate as many times as needed 
+		for(int i=0 ;i<n;i++){
+			//calculate result at each node
+			xmid= a+ (i+0.5)*h;
+			node1= xmid+ 0.5*h*x1;
+			node2= xmid+ 0.5*h*x2;
+			node3= xmid+ 0.5*h*x3;
+			node4= xmid+ 0.5*h*x4;
+			//update result
+			result+=w1*f(node1)+w2*f(node2)+w3*f(node3) + w4*f(node4);
+		}
 
-		xmid= a+ (i+0.5)*h;
-		node1= xmid+ 0.5*h*x1;
-		node2= xmid+ 0.5*h*x2;
-		node3= xmid+ 0.5*h*x3;
-		node4= xmid+ 0.5*h*x4;
-
-		result+=w1*f(node1)+w2*f(node2)+w3*f(node3) + w4*f(node4);
+		return (0.5*h*result);
 	}
-
-	return (0.5*h*result);
-}
--- Project4/test_int.cpp
+++ Project4/test_int.cpp
@@ -34,32 +34,30 @@ int main(){
 	f.c = 0.5;
 	f.d=25.0;
 	double Itrue = f.antiderivative(b) - f.antiderivative(a);
-  printf("\n True Integral = %22.16e\n", Itrue);
+	printf("\n True Integral = %22.16e\n", Itrue);
 
 
-  // test the Gauss-2 rule
-  cout << "\n Gauss-2 approximation:\n";
-  cout << "     n             R(f)            relerr    conv rate\n";
-  cout << "  ---------------------------------------------------\n";
-  vector<int> n = {20, 40, 80, 160, 320, 640, 1280, 2560, 5120, 10240};
-  vector<double> errors(n.size());
-  vector<double> hvals(n.size());
+	// test the Gauss-4 rule	
+	cout << "\n Gauss-4 approximation:\n";
+	cout << "     n             R(f)            relerr    conv rate\n";
+	cout << "  ---------------------------------------------------\n";
+	vector<int> n = {20, 40, 80, 160, 320, 640, 1280, 2560, 5120, 10240};
+	vector<double> errors(n.size());
+	vector<double> hvals(n.size());
 
-  // iterate over n values, computing approximations, error, convergence rate
-  double Iapprox;
-  for (int i=0; i<n.size(); i++) {
+	// iterate over n values, computing approximations, error, convergence rate
+	double Iapprox;
+	for (int i=0; i<n.size(); i++) {
 
-    printf("   %6i", n[i]);
-    Iapprox = composite_int(f, a, b, n[i]);
-    errors[i] = fabs(Itrue-Iapprox)/fabs(Itrue);
-    hvals[i] = (b-a)/n[i];
-    if (i == 0) 
-      printf("  %22.16e  %7.1e     ----\n", Iapprox, errors[i]);
-    else 
-      printf("  %22.16e  %7.1e   %f\n", Iapprox, errors[i], 
-	     (log(errors[i-1]) - log(errors[i]))/(log(hvals[i-1]) - log(hvals[i])));
-    
-  }
-  cout << "  ---------------------------------------------------\n";
-
+		printf("   %6i", n[i]);
+		Iapprox = composite_int(f, a, b, n[i]);
+		errors[i] = fabs(Itrue-Iapprox)/fabs(Itrue);
+		hvals[i] = (b-a)/n[i];
+		if (i == 0) 
+			printf("  %22.16e  %7.1e     ----\n", Iapprox, errors[i]);
+		else 
+		printf("  %22.16e  %7.1e   %f\n", Iapprox, errors[i], 
+			(log(errors[i-1]) - log(errors[i]))/(log(hvals[i-1]) - log(hvals[i])));
+	}
+	cout << "  ---------------------------------------------------\n";
 }
